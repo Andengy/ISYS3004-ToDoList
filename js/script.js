65f8fac01137d5b27267202d69c89b52dcbc5002
Andengy <127649084+Andengy@users.noreply.github.com>
2024-04-15 09:30:03 +0800

    added formatting to js and html

--- js/script.js
+++ js/script.js
@@ -5,13 +5,18 @@ console.log("script loaded");
 let addTaskButton = document.getElementById("add-button");
 let newTaskInput = document.getElementById("task-input");
 let todoContainer = document.getElementById("todo-list");
+let templateElement = document.getElementById("todo-item-template")
+let template = templateElement.innerHTML
 
 // 2. Write events
 function onAddTaskClicked(event){
     let taskName = newTaskInput.value;
     newTaskInput.value = ""
-    todoContainer.insertAdjacentHTML('afterbegin', taskName);
 
+    let todoHTML = template.replace("<!-- task_name -->", taskName)
+
+
+    todoContainer.insertAdjacentHTML('afterbegin', todoHTML);
 
 }
 
