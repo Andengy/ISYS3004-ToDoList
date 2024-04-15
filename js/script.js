67a018946d41d71d4f726e9a09ab4f45a394b7a9
Andengy <127649084+Andengy@users.noreply.github.com>
2024-04-15 08:56:41 +0800

    Added java script

--- js/script.js
+++ js/script.js
@@ -1,2 +1,19 @@
 console.log("script loaded");
 
+// 1. Write variables
+
+let addTaskButton = document.getElementById("add-button");
+let newTaskInput = document.getElementById("task-input");
+let todoContainer = document.getElementById("todo-list");
+
+// 2. Write events
+function onAddTaskClicked(event){
+    let taskName = newTaskInput.value;
+    newTaskInput.value = " "
+    todoContainer.insertAdjacentHTML('afterbegin',taskName)
+
+}
+
+// 3. Linking events and element to the function
+addTaskButton.addEventListener('click', onAddTaskClicked)
+
